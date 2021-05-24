c1f95d1978c907d8fa6f75f01a9c5dceb9aee6b9
paty6 <paty6@LAPTOP-PQNMHTVG>
2021-05-24 15:35:00 -0500

    ok

--- src/main/java/com/patricia/CalculadoraController.java
+++ src/main/java/com/patricia/CalculadoraController.java
@@ -14,7 +14,7 @@ public class CalculadoraController {
 	public String index(Model modelo)
 	{
 		modelo.addAttribute("calcu", new Calculadora());
-		return "/calculadora/index";
+		return "calculadora/index";
 		
 	}
 	@PostMapping("/resultado")
@@ -22,6 +22,6 @@ public class CalculadoraController {
 	{
 		calcu.Calcular();
 		modelo.addAttribute("calcu",calcu);
-		return "/calculadora/index";
+		return "calculadora/index";
 	}
 }

020d3a184ba54845faf9b9da7d713c770d54a48b
paty6 <paty6@LAPTOP-PQNMHTVG>
2021-05-24 15:25:32 -0500

    CargaInicio

---
+++ src/test/java/com/patricia/WebCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patricia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
